edbf24483207436a4930c5154ff1004fd86587ee
SK <sk@SKs-MacBook-Pro.local>
2023-10-01 18:53:34 +0530

    Added methodology and pdd indexing, retrieval and Q&A

--- bin/rag_index.py
+++ bin/rag_index.py
@@ -1,6 +1,10 @@
 import argparse
 import glob
 import logging
+from os import path
+
+from dotenv import load_dotenv
+import langchain
 
 from transist import index
 
@@ -14,28 +18,60 @@ SECTIONS = [
 ]
 
 
-def search_index(index_dir):
-    pdd_coll = index.PddCollection(index_dir, sections=SECTIONS)
-    results = pdd_coll.search_in_section("application of methodology", "stove", n=5)
+def search_index(pdd_index_dir, method_index_dir):
+    question = "what methodologies can be applied to projects involving cook stoves"
+    pdd_coll = index.PddLlamaCollection(pdd_index_dir)
+    results = pdd_coll.search(question, n=5)
     for result in results:
-        print(f"""text: {result.page_content}\n\nmetadata: {result.metadata}\n\n\n""")
+        print(f"""text: {result.text}\n\nmetadata: {result.metadata}\n\n\n""")
+
+    answer = pdd_coll.answer(question)
+    print("###### PDD ANSWER ######")
+    print(answer)
+    print("\n\n")
+
+    method_coll = index.MethodologyCollection(method_index_dir)
+    results = method_coll.search(question, n=5, mmr=True)
+    for result in results:
+        print(f"""result: {result.text}\n\n metadata:{result.metadata}""")
+
+    answer = method_coll.answer(question)
+    print("###### METHODOLOGY ANSWER ######")
+    print(answer)
 
 
-def make_index(pdd_dir, index_dir):
-    index.PddCollection.from_directories(glob.glob(f"{pdd_dir}/*"), index_dir,
-                                         sections=SECTIONS)
+def make_pdd_index(pdd_dir, pdd_index_dir):
+    if pdd_dir is not None:
+        index.PddLlamaCollection.from_directory(pdd_dir, pdd_index_dir)
+
+
+def make_methodology_index(method_dir, method_index_dir):
+    if method_dir is not None:
+        index.MethodologyCollection.from_directory(method_dir, method_index_dir)
 
 
 if __name__ == "__main__":
+    load_dotenv()
     logging.basicConfig(level=logging.INFO)
+    logging.getLogger("transist").setLevel(logging.DEBUG)
+
     parser = argparse.ArgumentParser(
         prog="rag_index",
         description="Manipulate Transistry RAG index"
     )
     parser.add_argument("-i", "--index_dir", action="store", required=True)
-    parser.add_argument("-d", "--data_dir", action="store", default=None, required=False)
+    parser.add_argument("-p", "--pdd_dir", action="store", default=None, required=False)
+    parser.add_argument("-m", "--method_dir", action="store", default=None, required=False)
 
     args = parser.parse_args()
-    if args.data_dir:
-        make_index(pdd_dir=args.data_dir, index_dir=args.index_dir)
-    search_index(args.index_dir)
+    langchain.verbose = True
+
+    if args.pdd_dir:
+        make_pdd_index(pdd_dir=args.pdd_dir,
+                       pdd_index_dir=path.join(args.index_dir, 'pdd'))
+    if args.method_dir:
+        make_methodology_index(method_dir=args.method_dir,
+                               method_index_dir=path.join(args.index_dir, 'method'))
+
+    search_index(path.join(args.index_dir, 'pdd'),
+                 path.join(args.index_dir, 'method'))
--- transist/index.py
+++ transist/index.py
@@ -2,27 +2,131 @@ import collections
 import glob
 import json
 import logging
-import random
 from os import path
 from pathlib import Path
 
 import chromadb
-import numpy as np
 from chromadb import Settings
-from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 
+from langchain.chains import RetrievalQA
 from langchain.document_loaders import TextLoader
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.llms import OpenAI
 from langchain.schema import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma, utils
-from typing import Dict, List, Sequence, Type
+from llama_index import ServiceContext, SimpleDirectoryReader, StorageContext, VectorStoreIndex
+from llama_hub.file.pdf.base import PDFReader
+from llama_index.embeddings import HuggingFaceEmbedding
+from llama_index.vector_stores import ChromaVectorStore
+
+from typing import Dict, List, Sequence
 
 
 log = logging.getLogger(__name__)
+chunklog = logging.getLogger(f"{__name__}.chunks")
 
 
 def canonical_section(section: str) -> str:
     return section.lower().replace(' ', '_')
+
+
+class BaseCollection(object):
+    def __init__(self, index_dir, collection_name):
+        self.index_dir = index_dir
+        self.client = chromadb.PersistentClient(
+            path=self.index_dir,
+            settings=Settings(anonymized_telemetry=False)
+        )
+        self.embed_model = "local"
+        self.vector_store = ChromaVectorStore(
+            chroma_collection=self.client.get_or_create_collection(collection_name)
+        )
+        self.index = None
+
+    def answer(self, query):
+        if not self.index:
+            self.index = VectorStoreIndex.from_vector_store(
+                vector_store=self.vector_store,
+                service_context=ServiceContext.from_defaults(embed_model=self.embed_model)
+            )
+        return self.index.as_query_engine(similarity_top_k=5).query(query)
+
+    def search(self, query, n=5, mmr=False):
+        if not self.index:
+            self.index = VectorStoreIndex.from_vector_store(
+                vector_store=self.vector_store,
+                service_context=ServiceContext.from_defaults(embed_model=self.embed_model)
+            )
+        return self.index.as_retriever(similarity_top_k=n).retrieve(query)
+
+
+class MethodologyCollection(BaseCollection):
+    def __init__(self, index_dir):
+        super().__init__(index_dir=index_dir, collection_name="methodology_index")
+
+    @classmethod
+    def from_directory(cls, methodology_dir, index_dir):
+        instance = cls(index_dir=index_dir)
+        instance.index_directory(methodology_dir)
+        return instance
+
+    def index_directory(self, methodology_dir):
+        loader = SimpleDirectoryReader(input_dir=methodology_dir,
+                                       required_exts=[".pdf"],
+                                       file_extractor={'.pdf': PDFReader()})
+
+        documents = loader.load_data()
+        log.info("Indexing %i documents in index", len(documents))
+        self.index = VectorStoreIndex.from_documents(
+            documents=documents,
+            storage_context=StorageContext.from_defaults(vector_store=self.vector_store),
+            service_context=ServiceContext.from_defaults(embed_model=self.embed_model),
+            show_progress=True
+        )
+        log.debug("Done!")
+
+
+class PddLlamaCollection(BaseCollection):
+    def __init__(self, index_dir):
+        super().__init__(index_dir=index_dir, collection_name="pdd_index")
+
+    @classmethod
+    def from_directory(cls, pdd_dir, index_dir):
+        instance = cls(index_dir=index_dir)
+        instance.index_directories(pdd_dir)
+        return instance
+
+    def index_directories(self, pdd_dir):
+        loader = SimpleDirectoryReader(input_dir=pdd_dir,
+                                       required_exts=[".txt"],
+                                       recursive=True,
+                                       file_metadata=pdd_section_file_metadata)
+        documents = loader.load_data()
+        log.info("Indexing %i documents in index", len(documents))
+        self.index = VectorStoreIndex.from_documents(
+            documents=documents,
+            storage_context=StorageContext.from_defaults(vector_store=self.vector_store),
+            service_context=ServiceContext.from_defaults(embed_model=self.embed_model),
+            show_progress=True
+        )
+        log.debug("Done!")
+
+
+def section_from_file_name(file_name: str) -> str:
+    base_file_name, section_str = file_name.rsplit('_', 1)
+    section_name: str = section_str.split(' ', 1)[1].split('.', 1)[0]
+    return canonical_section(section_name)
+
+
+def pdd_section_file_metadata(filename: str):
+    section = section_from_file_name(path.basename(filename))
+    meta_filepath = filename.strip('.txt') + '.json'
+    metadata = json.loads(Path(meta_filepath).read_text())
+    metadata.update({
+        'section_name': section
+    })
+    return metadata
 
 
 class PddCollection(object):
@@ -31,13 +135,20 @@ class PddCollection(object):
     Supports searching the collection constrained by section as well as other
     metadata constraints based on document metadata.
     """
-    def __init__(self, index_dir, sections: List[str]):
-        canonical_sections = map(canonical_section, sections)
-        settings = Settings(anonymized_telemetry=False)
-        self.pdd_indexes: Dict[str, SectionIndex] = {
-            section: SectionIndex(section, chromadb.PersistentClient(path.join(index_dir, section), settings))
-            for section in canonical_sections
-        }
+    def __init__(self, index_dir, sections):
+        self.index_dir = index_dir
+        self._pdd_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=500,
+            chunk_overlap=50,
+        )
+        self.embedding = SentenceTransformerEmbeddings(model_name="all-mpnet-base-v2")
+        self.persistent_client = chromadb.PersistentClient(self.index_dir, Settings(anonymized_telemetry=False))
+        self.pdd_index = Chroma(
+            collection_name="pdd_index",
+            embedding_function=self.embedding,
+            persist_directory=self.index_dir,
+            client=self.persistent_client,
+            collection_metadata={"hnsw:space": "cosine"})
 
     @classmethod
     def from_directories(cls, pdd_dirs: List[str], index_dir: str, sections: List[str]):
@@ -57,94 +168,50 @@ class PddCollection(object):
 
         :param pdd_dirs: List of directories, one per PDD
         """
+        files_by_section, metadata_by_section = self.files_by_section(pdd_dirs)
+        for section in map(canonical_section, files_by_section):
+            log.info("Indexing section: %s", section)
+
+            section_chunks = []
+            for filepath, meta_filepath in zip(files_by_section[section], metadata_by_section[section]):
+                metadata = json.loads(Path(meta_filepath).read_text())
+                loader = PddSectionLoader(section, filepath, metadata)
+                doc_chunks = self._pdd_splitter.split_documents(loader.load())
+                log.debug("Got %i chunks from %s", len(doc_chunks), filepath)
+                section_chunks.extend(doc_chunks)
+
+            log.info("Got %i chunks to index for section %s", len(section_chunks), section)
+            self.pdd_index.add_documents(section_chunks)
+
+        self.pdd_index.persist()
+
+    @staticmethod
+    def files_by_section(pdd_dirs: List[str]):
         files_by_section = collections.defaultdict(list)
         metadata_by_section = collections.defaultdict(list)
 
         for pdd_dir in pdd_dirs:
             log.info("Indexing PDD from directory: %s", pdd_dir)
             for text_file in glob.glob(f"{pdd_dir}/sections/*.txt"):
-                section = self._section_from_file_name(path.basename(text_file))
+                section = section_from_file_name(path.basename(text_file))
                 files_by_section[section].append(text_file)
 
             for metadata_file in glob.glob(f"{pdd_dir}/sections/*.json"):
-                section = self._section_from_file_name(path.basename(metadata_file))
+                section = section_from_file_name(path.basename(metadata_file))
                 metadata_by_section[section].append(metadata_file)
+        return files_by_section, metadata_by_section
 
-        for section in files_by_section:
-            try:
-                self.pdd_indexes[section].index_filepaths(filepaths=files_by_section[section],
-                                                          metadatapaths=metadata_by_section[section])
-            except KeyError:
-                log.warning("Unknown section %s, skipping %d pdd files",
-                            section, len(files_by_section[section]))
-
-    def search_in_section(self, section, query, **kwargs) -> Sequence[Document]:
-        return self.pdd_indexes[canonical_section(section)].search(query, **kwargs)
-
-    @staticmethod
-    def _section_from_file_name(file_name: str) -> str:
-        base_file_name, section_str = file_name.rsplit('_', 1)
-        section_name: str = section_str.split(' ', 1)[1].split('.', 1)[0]
-        return canonical_section(section_name)
-
-
-class SectionIndex(object):
-    def __init__(self, section_name, index_client: chromadb.API):
-        self.section_name = section_name
-        self._pdd_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=500,
-            chunk_overlap=20,
+    def search_in_section(self, section, query, n=5, mmr=True) -> Sequence[Document]:
+        db = self.pdd_index
+        section_retriever = db.as_retriever(
+            search_type="mmr" if mmr else "similarity",
+            search_kwargs={
+                'filter': {'section_name': canonical_section(section)},
+                'k': 5,
+                'fetch_k': int(n*2)
+            }
         )
-        self.client = index_client
-        self.collection = self.client.get_or_create_collection(
-            name=f"chromadb-{self.section_name}",
-            metadata={"hnsw:space": "cosine"},
-            embedding_function=SentenceTransformerEmbeddingFunction(model_name="multi-qa-mpnet-base-dot-v1")
-        )
-
-    def index_filepaths(self, filepaths: List[str], metadatapaths: List[str]):
-        for filepath, metadatapath in zip(filepaths, metadatapaths):
-            metadata = json.loads(Path(metadatapath).read_text())
-            loader = PddSectionLoader(self.section_name, filepath, metadata)
-            chunks = self._pdd_splitter.split_documents(loader.load())
-            if random.random() < 0.2:
-                for i in range(10):
-                    log.debug("Chunk: %s", random.choice(chunks))
-
-            ids = [f"{filepath}:{i}" for i in range(len(chunks))]
-            metadatas = [c.metadata for c in chunks]
-            texts = [c.page_content for c in chunks]
-            self.collection.upsert(ids=ids,
-                                   metadatas=metadatas,
-                                   documents=texts)
-            log.info("Upserted %d chunks from: '%s'", len(chunks), filepath)
-
-    def search(self, query: str, where_meta=None, n: int = 10, **kwargs) -> Sequence[Document]:
-        log.info("Searching %s collection", self.section_name)
-        results = self.collection.query(
-            query_texts=[query],
-            n_results=int(n*1.5),
-            where=where_meta,
-            include=["metadatas", "documents", "embeddings"],
-            **kwargs
-        )
-
-        candidates = [
-            Document(page_content=d, metadata=m or {})
-            for (d, m) in zip(results["documents"][0], results["metadatas"][0])
-        ]
-        log.info("Got %i candidate items", len(candidates))
-
-        query_embeddings = self.collection._embedding_function([query])
-        mmr_selected = utils.maximal_marginal_relevance(
-            np.array(query_embeddings[0], np.float32),
-            results["embeddings"][0],
-            k=n,
-            lambda_mult=0.5
-        )
-        log.info("Selected %i most diverse results", len(mmr_selected))
-
-        return [doc for (i, doc) in enumerate(candidates) if i in mmr_selected]
+        return section_retriever.get_relevant_documents(query)
 
 
 class PddSectionLoader(TextLoader):
@@ -161,5 +228,3 @@ class PddSectionLoader(TextLoader):
             'section_name': self.section_name
         })
         return docs
-
-
