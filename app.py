25f3080606082f6f5ac951c2e3f98b70ab66b3ba
SK <sk@SKs-MacBook-Pro.local>
2023-10-10 15:25:41 +0530

    Added new state to the PDD designer handling explore mode.

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from langchain.prompts.chat import (
 import pydantic.v1.error_wrappers
 from typing import Any, Dict
 
-from transist.llm import create_llm, parse_json_maybe_invalid
+from transist.llm import create_openai_llm, parse_json_maybe_invalid, ExtractionOutputParser
 from transist.prompt import system_prompt, draft_question_prompt, extract_facts_prompt
 
 logging.basicConfig(level=logging.INFO)
@@ -26,63 +26,88 @@ thinking = [
 
 class CarbonAssistant(object):
     def __init__(self):
-        self.llm = None
         self.state = "extract"
+        self.extract_parser = ExtractionOutputParser()
         self.waste_management_baseline_json = open("waste_management_baseline_template.json", "r").read()
-        self.waste_management_baseline_facts: Dict = json.loads(self.waste_management_baseline_json)
+        self.baseline_facts: Dict = json.loads(self.waste_management_baseline_json)
         self.sufficient_facts_response = "Sufficient facts to generate"
+        self.current_questions = []
 
     def design(self, message, history, openai_api_key=None):
         try:
-            self.llm = create_llm(openai_api_key)
+            llm = create_openai_llm(openai_api_key)
+            for out in self.design_with_llm(llm, message, history):
+                yield out
         except pydantic.v1.error_wrappers.ValidationError as e:
             if any(["OPENAI_API_KEY" in error['msg']for error in e.errors()]):
                 raise gr.Error("An OpenAI API key needs to be provided in the Additional Inputs section below")
             else:
                 raise gr.Error(pydantic.v1.error_wrappers.display_errors(e.errors()))
 
+    def design_with_llm(self, llm, message, history):
         if self.state == "draft":
-            questions = self.draft_questions(self.waste_management_baseline_facts)
+            questions = self.draft_questions(llm, self.baseline_facts)
             if self.sufficient_to_generate(questions):
+                self.current_questions = []
                 self.state = "generate"
                 yield self.sufficient_facts_response
             else:
+                self.current_questions = questions
                 self.state = "extract"
                 yield "Let's continue gathering information about your carbon project"
                 yield questions
         elif self.state == "extract":
             yield f"Thank you for providing information about your project. {random.choice(thinking)}"
 
-            extracted = self.extract_facts(message, history, self.waste_management_baseline_facts)
-            self.waste_management_baseline_facts.update(extracted)
-            log.info("Updated facts doc: %s", self.waste_management_baseline_facts)
-            self.state = "draft"
-            for out in self.design(message, history, openai_api_key):
-                yield out
+            extracted = self.extract_facts(llm, message, history, self.baseline_facts)
+            if extracted.get("keys_updated", []):
+                extracted_facts = extracted.get("extracted_project_facts", {})
+                self.baseline_facts.update(extracted_facts)
+                log.info("Updated facts doc: %s", self.baseline_facts)
+                self.state = "draft"
+            else:
+                self.state = "explore"
 
-    def draft_questions(self, facts_document):
+            for out in self.design_with_llm(llm, message, history):
+                yield out
+        elif self.state == "explore":
+            yield "I understand that you need some help in answering these questions."
+            yield "Give me a moment to try and find some relevant information which can help."
+            explore_results = self.explore(llm, message, history, self.baseline_facts)
+            self.state = "extract"
+            yield explore_results
+
+    def draft_questions(self, llm, facts_document):
         questions_chain = LLMChain(
-            llm=self.llm,
+            llm=llm,
             prompt=ChatPromptTemplate.from_messages([system_prompt, draft_question_prompt]),
             output_key="questions",
             verbose=True)
         questions = questions_chain.predict(json_template=json.dumps(facts_document))
         return questions
 
-    def extract_facts(self, message, history, facts_document) -> Dict[Any, Any]:
+    def extract_facts(self, llm, message, history, facts_document) -> Dict[Any, Any]:
         extract_chain = LLMChain(
-            llm=self.llm,
+            llm=llm,
             prompt=ChatPromptTemplate.from_messages([system_prompt, extract_facts_prompt]),
-            output_key="extracted_facts",
+            output_parser=self.extract_parser,
+            output_key="extracted",
             verbose=True)
 
-        extracted_facts_str = extract_chain.predict(project_facts_document=json.dumps(facts_document),
-                                                    project_information=message)
-        if not extracted_facts_str:
-            log.warning("Could not get extracted facts from extract chain: '%s'", extracted_facts_str)
-            extracted_facts_str = "{}"
+        extracted: Dict[str, Any] = extract_chain.predict_and_parse(
+            project_facts_document=json.dumps(facts_document),
+            project_information=message)
 
-        return parse_json_maybe_invalid(extracted_facts_str)
+        if not extracted:
+            log.warning("Could not extracted using extract chain: '%s'", extracted)
+
+        return extracted
+
+    def explore(self, llm, message, history, facts_document):
+        return f"""Some relevant search results to\n\nUser: {message}
+        
+        In context of \nhistory: 
+        {history}"""
 
     @staticmethod
     def sufficient_to_generate(drafted_questions) -> bool:
@@ -94,10 +119,10 @@ def main():
     openai_api_key = gr.Textbox(placeholder="Please enter you OpenAI API key here",
                                 label="Open AI API Key", render=False)
     demo = gr.ChatInterface(
-        title="CDM PDD Baseline Assistant",
+        title="Verra Carbon Project Design Assistant",
         description="""
         I'm a virtual assistant who can help you in writing the baseline section for 
-        your carbon project to be registered with the CDM registry. Please start by 
+        your carbon project to be registered with the Verra registry. Please start by 
         telling me something about your project.
         """,
         textbox=gr.Textbox(placeholder="Start by telling me about your project",
@@ -113,4 +138,5 @@ def main():
 
 if __name__ == "__main__":
     # Take environment variables from .env file
+    load_dotenv()
     main()
--- transist/llm.py
+++ transist/llm.py
@@ -1,14 +1,16 @@
 import json
 import logging
+from typing import Any, Dict
 
 from langchain.llms import OpenAI
-
+from langchain.output_parsers.json import parse_and_check_json_markdown
+from langchain.schema import BaseOutputParser, OutputParserException
 
 log = logging.getLogger(__name__)
 
 
-def create_llm(openai_api_key=None):
-    kwargs = dict(temperature=0, max_tokens=1536)
+def create_openai_llm(openai_api_key=None):
+    kwargs = dict(temperature=0, max_tokens=2400)
     if openai_api_key:
         kwargs['openai_api_key'] = openai_api_key
     return OpenAI(**kwargs)
@@ -26,3 +28,22 @@ def parse_json_maybe_invalid(json_as_str: str):
         except json.JSONDecodeError as e:
             log.error("Could not parse extracted facts string '%s' as JSON. Error: %s", json_as_str, e)
             return {}
+
+
+class ExtractionOutputParser(BaseOutputParser[Dict[str, str]]):
+    """Parse for output of extraction chain."""
+
+    def parse(self, text: str) -> Dict[str, Any]:
+        try:
+            expected_keys = ["extracted_project_facts", "keys_updated"]
+            parsed = parse_and_check_json_markdown(text, expected_keys)
+            if not isinstance(parsed["extracted_project_facts"], object):
+                raise ValueError("Expected 'extracted_project_facts' to be an object")
+            if not isinstance(parsed["keys_updated"], list):
+                raise ValueError("Expected 'keys_updated' to be a list")
+
+            return parsed
+        except Exception as e:
+            raise OutputParserException(
+                f"Parsing text=\n{text}\n raise following error: \n{e}"
+            ) from e
